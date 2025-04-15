0a96317d606d95d1e16303351d59c4c607a5f902
Babul Mirdha <babumirdha@gmail.com>
2025-04-15 20:03:08 +0600

    refacotred

--- rest-api-with-mongo/createIndex.js
+++ rest-api-with-mongo/createIndex.js
@@ -5,8 +5,6 @@ const Category = require('./models/Category'); // your category model
 
 async function ensureIndex() {
 
-  console.log(process.env.MONGO_URI);
-  
 
   await mongoose.connect(process.env.MONGO_URI);
   await Category.collection.createIndex({ name: 1 }, { unique: true });
